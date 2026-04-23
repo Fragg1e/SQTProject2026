031b67bf29a4d958e18302b84a54a0c1e8454fd6
Fragg1e <woodwardfraser1@gmail.com>
2026-04-23 16:33:37 +0100

    Update Program.cs

--- ParkingFeeCalculator.Web/Program.cs
+++ ParkingFeeCalculator.Web/Program.cs
@@ -1,28 +1,19 @@
-using Microsoft.AspNetCore.DataProtection;
 using ParkingFeeCalculator;
-//startup file and launch file
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Logging.ClearProviders();
-builder.Logging.AddConsole();
-builder.Logging.AddDebug();
-
-builder.Services.AddRazorPages(); //makes .cshtml files visible to the app
-builder.Services
-    .AddDataProtection()
-    .PersistKeysToFileSystem(new DirectoryInfo(Path.Combine(builder.Environment.ContentRootPath, "App_Data", "Keys"))); //config for encryption keys and stores
-//dependency injection
+builder.Services.AddRazorPages();
 builder.Services.AddSingleton<IDiscountService, DiscountService>();
 builder.Services.AddScoped<IParkingService, ParkingService>();
 
 var app = builder.Build();
-//if a request fails users see a friendly error page instead of the raw error
-if (!app.Environment.IsDevelopment()) 
+
+if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
     app.UseHsts();
 }
-//request pipeline (ORDER MATTERS THE ORDER REALLY MATTERS)
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
